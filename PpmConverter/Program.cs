a7c075867179a616e4ea7777aa2bfe58c5c21d43
Vierling, Marvin <marvin.vierling@student.fhws.de>
2016-10-26 14:48:24 +0200

    refactoring

--- PpmConverter/Program.cs
+++ PpmConverter/Program.cs
@@ -14,12 +14,14 @@ namespace PpmConverter
             {
                 string path = args[0];
                 PPMImage<RGBImage> image;
-                YCrCbImage ycrcbMatrix;
+                YCbCrImage ycrcbMatrix;
                 try
                 {
                     Console.Write("Load file... ");
                     image = PPMImage<RGBImage>.LoadImageFromFile(path);
-                    ycrcbMatrix = RGBToYCbCr(image.Matrix);
+                    Console.WriteLine(" - DONE");
+                    Console.Write("Converting to YCbCr...");
+                    ycrcbMatrix = YCbCrImage.FromRGB(image.Matrix);
                     Console.WriteLine(" - DONE");
                 }
                 catch (Exception ex)
@@ -30,35 +32,6 @@ namespace PpmConverter
 
                 Console.ReadLine();
             }
-        }
-
-        private static YCrCbImage RGBToYCbCr(RGBImage matrix)
-        {
-            byte[,] red = matrix.R;
-            byte[,] green = matrix.G;
-            byte[,] blue = matrix.B;
-
-            int maxWidth = matrix.R.GetLength(0);
-            int maxHight = matrix.R.GetLength(1);
-
-            byte[,] y = new byte[maxWidth, maxHight];
-            byte[,] cb = new byte[maxWidth, maxHight];
-            byte[,] cr = new byte[maxWidth, maxHight];
-
-            int width;
-            int hight;
-
-            for(hight = 0; hight < maxHight; hight++)
-            {
-                for (width = 0; width < maxWidth; width++)
-                {
-                    y[width, hight] = (byte)(((0.299 * red[width, hight]) + (0.587 * green[width, hight]) + (0.114 * blue[width, hight])));
-                    cb[width, hight] = (byte)(((-0.1687 * red[width, hight]) + (-0.3312 * green[width, hight]) + (0.5 * blue[width, hight])) + 128);
-                    cr[width, hight] = (byte)(((0.5 * red[width, hight]) + (-0.4186 * green[width, hight]) + (-0.0813 + blue[width, hight])) + 128);
-                }
-            }
-
-            return new YCrCbImage(y, cb, cr);
         }
     }
 }
---
+++ PpmConverter/YCbCrImage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PpmConverter
+{
+    class YCbCrImage : Image
+    {
+        private byte[,] _y;
+        private byte[,] _cr;
+        private byte[,] _cb;
+
+        public YCbCrImage(byte[,] y, byte[,] cb, byte[,] cr)
+        {
+            _y = y;
+            _cb = cb;
+            _cr = cr;
+        }
+
+        public YCbCrImage(int x, int y)
+        {
+            _y = new byte[x, y];
+            _cb = new byte[x, y];
+            _cr = new byte[x, y];
+        }
+
+        public byte[,] Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public byte[,] Cr
+        {
+            get { return _cr; }
+            set { _cr = value; }
+        }
+
+        public byte[,] Cb
+        {
+            get { return _cb; }
+            set { _cb = value; }
+        }
+
+        public static YCbCrImage FromRGB(RGBImage rgbImage)
+        {
+            byte[,] red = rgbImage.R;
+            byte[,] green = rgbImage.G;
+            byte[,] blue = rgbImage.B;
+
+            int maxWidth = rgbImage.R.GetLength(0);
+            int maxHeight = rgbImage.R.GetLength(1);
+
+            byte[,] y = new byte[maxWidth, maxHeight];
+            byte[,] cb = new byte[maxWidth, maxHeight];
+            byte[,] cr = new byte[maxWidth, maxHeight];
+
+
+            for (int height = 0; height < maxHeight; height++)
+            {
+                for (int width = 0; width < maxWidth; width++)
+                {
+                    y[width, height] = (byte)(((0.299 * red[width, height]) + (0.587 * green[width, height]) + (0.114 * blue[width, height])));
+                    cr[width, height] = (byte)(((0.5 * red[width, height]) + (-0.4186 * green[width, height]) + (-0.0813 + blue[width, height])) + 128);
+                    cb[width, height] = (byte)(((-0.1687 * red[width, height]) + (-0.3312 * green[width, height]) + (0.5 * blue[width, height])) + 128);
+                }
+            }
+
+            return new YCbCrImage(y, cb, cr);
+        }
+    }
+}
