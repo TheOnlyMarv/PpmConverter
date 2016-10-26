090fecab049d428f84f51db921786e22f91e9af5
Pascal <mueller.pascal@student.fhws.de>
2016-10-26 13:06:12 +0200

    Fehler beseitigt

--- PpmConverter/Program.cs
+++ PpmConverter/Program.cs
@@ -38,20 +38,23 @@ namespace PpmConverter
             byte[,] green = matrix.G;
             byte[,] blue = matrix.B;
 
-            byte[,] y = new byte[matrix.R.GetLength(0), matrix.R.GetLength(1)];
-            byte[,] cb = new byte[matrix.R.GetLength(0), matrix.R.GetLength(1)];
-            byte[,] cr = new byte[matrix.R.GetLength(0), matrix.R.GetLength(1)];
+            int maxWidth = matrix.R.GetLength(0);
+            int maxHight = matrix.R.GetLength(1);
 
-            byte width;
-            byte hight;
+            byte[,] y = new byte[maxWidth, maxHight];
+            byte[,] cb = new byte[maxWidth, maxHight];
+            byte[,] cr = new byte[maxWidth, maxHight];
 
-            for(hight = 0; hight < matrix.R.GetLength(1); hight++)
+            int width;
+            int hight;
+
+            for(hight = 0; hight < maxHight; hight++)
             {
-                for (width = 0; width < matrix.R.GetLength(0); width++)
+                for (width = 0; width < maxWidth; width++)
                 {
-                    y[hight, width] = (byte)(((0.299 * red[width, hight]) + (0.587 * green[width, hight]) + (0.114 * blue[width, hight])));
-                    cb[hight, width] = (byte)(((-0.1687 * red[width, hight]) + (-0.3312 * green[width, hight]) + (0.5 * blue[width, hight])) + 128);
-                    cr[hight, width] = (byte)(((0.5 * red[width, hight]) + (-0.4186 * green[width, hight]) + (-0.0813 + blue[width, hight])) + 128);
+                    y[width, hight] = (byte)(((0.299 * red[width, hight]) + (0.587 * green[width, hight]) + (0.114 * blue[width, hight])));
+                    cb[width, hight] = (byte)(((-0.1687 * red[width, hight]) + (-0.3312 * green[width, hight]) + (0.5 * blue[width, hight])) + 128);
+                    cr[width, hight] = (byte)(((0.5 * red[width, hight]) + (-0.4186 * green[width, hight]) + (-0.0813 + blue[width, hight])) + 128);
                 }
             }
 
