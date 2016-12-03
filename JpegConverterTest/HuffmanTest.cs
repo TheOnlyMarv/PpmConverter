52d66feb5572a196dd94cbd8f8b0d9a3b6ba2904
Vierling, Marvin <marvinvierling@hotmail.de>
2016-12-03 20:23:49 +0100

    test for right huffman tree

---
+++ JpegConverterTest/HuffmanTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JpegConverter.Huffman;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace JpegConverterTest
+{
+    [TestClass]
+    public class HuffmanTest
+    {
+        [TestMethod]
+        public void TestNormalHuffman()
+        {
+            SortedList<float, int> sl = new SortedList<float, int>(new DuplicateKeyComparer<float>());
+            sl.Add(4f, 1);
+            sl.Add(4f, 2);
+            sl.Add(6f, 3);
+            sl.Add(6f, 4);
+            sl.Add(7f, 5);
+            sl.Add(9f, 6);
+            HuffmanTree ht = HuffmanTree.createNormalTree(sl);
+
+            Assert.AreEqual("010", ht.findCode(1, ""));
+            Assert.AreEqual("011", ht.findCode(2, ""));
+            Assert.AreEqual("110", ht.findCode(3, ""));
+            Assert.AreEqual("111", ht.findCode(4, ""));
+            Assert.AreEqual("00", ht.findCode(5, ""));
+            Assert.AreEqual("10", ht.findCode(6, ""));
+        }
+
+        [TestMethod]
+        public void TestRightHuffman()
+        {
+            SortedList<float, int> sl = new SortedList<float, int>(new DuplicateKeyComparer<float>());
+            sl.Add(4f, 1);
+            sl.Add(4f, 2);
+            sl.Add(6f, 3);
+            sl.Add(6f, 4);
+            sl.Add(7f, 5);
+            sl.Add(9f, 6);
+            HuffmanTree ht = HuffmanTree.createRightTree(sl);
+
+            Assert.AreEqual("100", ht.findCode(1, ""));
+            Assert.AreEqual("101", ht.findCode(2, ""));
+            Assert.AreEqual("110", ht.findCode(3, ""));
+            Assert.AreEqual("111", ht.findCode(4, ""));
+            Assert.AreEqual("00", ht.findCode(5, ""));
+            Assert.AreEqual("01", ht.findCode(6, ""));
+        }
+    }
+}
