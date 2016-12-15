5d942f69005dc67d15a954bb8527f49fb4fb213a
Pascal <mueller.pascal@student.fhws.de>
2016-12-15 18:50:28 +0100

    seperierteKosinusTransformation entählt einen Fehler, gefunden noch nicht

--- PpmConverter/DCTPascal/KosinusTransformation.cs
+++ PpmConverter/DCTPascal/KosinusTransformation.cs
@@ -39,7 +39,7 @@ namespace JpegConverter.DCTPascal
             }
             double sumsum = 0;
 
-            int[,] dctBlock = new int[8, 8];
+            int[,] dctBlock = new int[size, size];
             for(int i = 0; i < size; i++)
             {
                 for(int j = 0; j < size; j++)
@@ -92,42 +92,52 @@ namespace JpegConverter.DCTPascal
 
             int[,] matrix = new int[size, size];
             int[,] matrixT = new int[size, size];
+            int[,] axat = new int[size, size];
             int[,] sDCT = new int[size, size];
 
-            for(int k = 0; k < size; k++)
+            for (int n = 0; n < size; n++)
             {
-                for(int n = 0; n < size; n++)
+                for (int k = 0; k < size; k++)
                 {
-                    if(k == 0)
+                    if (k == 0)
                     {
-                        matrix[k, n] = (int)(Math.Round(1.0 / Math.Sqrt(2) * Math.Sqrt(2 / size) * Math.Cos((2.0 * n + 1.0) * (k * Math.PI / (2.0 * size)))));
-                        matrixT[n, k] = matrix[k, n];
+                        matrix[n, k] = (int)(Math.Round(1.0 / Math.Sqrt(2) * Math.Sqrt(2.0 / size) * Math.Cos((2.0 * n + 1.0) * (k * Math.PI / (2.0 * size)))));
+                        matrixT[k, n] = matrix[n, k];
                     }
                     else
                     {
-                        matrix[k, n] = (int)(Math.Round(1.0 * Math.Sqrt(2 / size) * Math.Cos((2.0 * n + 1.0) * (k * Math.PI / (2.0 * size)))));
-                        matrixT[n, k] = matrix[k, n];
+                        matrix[n, k] = (int)(Math.Round(1.0 * Math.Sqrt(2.0 / size) * Math.Cos((2.0 * n + 1.0) * (k * Math.PI / (2.0 * size)))));
+                        matrixT[k, n] = matrix[n, k];
                     }
                 }
             }
+
+            axat = matrizenMultiplikation(matrix, matrixT);
+            sDCT = matrizenMultiplikation(axat, block);
+
+            return sDCT;
+        }
+
+        private static int[,] matrizenMultiplikation( int[,] matrix, int[,] matrix2)
+        {          
+            int size = matrix.GetLength(0);
+            int[,] newMatrix = new int[size, size];
             int produkt = 0;
             for (int x = 0; x < size; x++)
             {
                 for (int y = 0; y < size; y++)
                 {
-                    for (int i = 0; i < size; i++)
-                    {
                         for (int j = 0; j < size; j++)
                         {
-                            produkt = produkt + matrix[i, j] * matrixT[j, i];
+                            produkt = produkt + matrix[x, j] * matrix2[j, y];
                         }
+                        newMatrix[x, y] = produkt;
+                        produkt = 0;
                     }
-                    sDCT[x, y] = produkt;
-                    produkt = 0;
-                }
             }
-            return sDCT;
-        } 
+
+            return newMatrix;
+        }
 
         public static int[,] inverseDiskreteKosinusTransformation(int[,] block)
         {
